b3d3df3aadc92718942c840f4df99da16dd96b3a
Eubank Wang <dimply-roaring.5x@icloud.com>
2026-03-27 14:54:43 -0700

    Generate *.deb or *.rpm based on distro

--- forge.config.js
+++ forge.config.js
@@ -1,4 +1,47 @@
 const path = require("path")
+const fs = require("fs")
+
+function getOsRelease() {
+    try {
+        return fs.readFileSync("/etc/os-release", "utf-8").toLowerCase()
+    } catch (_e) {
+        return ""
+    }
+}
+
+function isDebDistro() {
+    const release = getOsRelease()
+    return /\b(ubuntu|debian|mint|pop|elementary|zorin|kali|raspbian|mx\b|antix|deepin|lmde)/.test(release)
+}
+
+function isRpmDistro() {
+    const release = getOsRelease()
+    return /\b(fedora|rhel|centos|rocky|alma|opensuse|suse|amzn|ol)\b/.test(release)
+}
+
+function getLinuxMaker() {
+    if (process.platform !== "linux") return null
+
+    const iconPath = path.join(__dirname, "assets", "icon.png")
+    if (isDebDistro()) {
+        return {
+            name: "@electron-forge/maker-deb",
+            config: { mimeType: ["text/markdown"], options: { name: "mdv", icon: iconPath } }
+        }
+    }
+    if (isRpmDistro()) {
+        return {
+            name: "@electron-forge/maker-rpm",
+            config: { mimeType: ["text/markdown"], options: { name: "mdv", icon: iconPath } }
+        }
+    }
+    throw new Error(
+        "Unsupported Linux distribution: could not detect a deb or rpm based distro from /etc/os-release. " +
+            "Please add your distro to forge.config.js."
+    )
+}
+
+const linuxMaker = getLinuxMaker()
 
 module.exports = {
     packagerConfig: {
@@ -30,16 +73,7 @@ module.exports = {
             name: "@electron-forge/maker-zip",
             platforms: ["darwin"]
         },
-        {
-            name: "@electron-forge/maker-deb",
-            config: {
-                mimeType: ["text/markdown"],
-                options: {
-                    name: "mdv",
-                    icon: path.join(__dirname, "assets", "icon.png")
-                }
-            }
-        },
+        ...(linuxMaker ? [linuxMaker] : []),
         {
             name: "@electron-forge/maker-zip",
             platforms: ["linux"]
